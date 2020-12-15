24225f86d65a065a90a660afe684e35f6984f6b8
GhassenSmaoui <smaouighassen@yahoo.fr>
2020-12-15 18:30:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Form, Button} from 'react-bootstrap';
+import natureImage from "./nature.jpg"
 
 function App() {
   return (
     <div className="App">
       
-      <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+      <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+ <h1 className="title red">Your name here</h1>
+
+ <br/>
+
+ <img src={natureImage}/>
+
+ <br/>
+
+ <img src="./wings.jpg"/>
+
+</div>
+
+<video width="320" height="240" controls>
+
+ <source src="myVideo.mp4" type="video/mp4" />
+
+</video>
       
       
     </div>
